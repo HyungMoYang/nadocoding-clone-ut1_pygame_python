fa27c219b1cb6f3746b9bbd83335c0778c833692
TipsyBoy <gudah13@naver.com>
2020-09-08 03:24:48 +0900

    8_frame.py

---
+++ pygame_basic/8_frame.py
@@ -0,0 +1,43 @@
+##################################################################################
+import pygame
+##################################################################################
+# 기본 초기화 (반드시 해야 하는 것들)
+pygame.init()  # pygame 초기화 (반드시 필요)
+
+# 화면 크기 설정
+screen_width = 480  # 가로 크기
+screen_height = 640  # 세로 크기
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+# 화면 타이틀 설정 - 윈도우 타이틀
+pygame.display.set_caption("게임 이름")  # 게임 타이틀
+
+# FPS
+clock = pygame.time.Clock()
+##################################################################################
+
+# 1. 사용자 게임 초기화(배경 화면, 게임 이미지, 좌표, 폰트, 등)
+
+# 배경 이미지 불러오기, 경로에 '\' 신경쓸 것
+background = pygame.image.load()
+
+
+running = True
+while running:
+    dt = clock.tick(60)
+
+    # 2. 이벤트 처리(키보드, 마우스 등)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # 3. 게임 캐릭터 위치 정의
+
+    # 4. 충돌 처리
+
+    # 5. 화면에 그리기
+
+    pygame.display.update()  # 게임화면을 다시 그리기 - pygame에선 배경을 계속 업데이트 해주어야함
+
+# pygame 종료
+pygame.quit()
